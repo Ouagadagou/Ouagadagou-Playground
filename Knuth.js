09f64a7023393d3c5f383ab19ae2f520bd728f2b
Ouagadagou <mh.habti@gmail.com>
2023-02-02 21:58:04 -0500

    Quick update

--- Knuth.js
+++ Knuth.js
@@ -1,3 +1,4 @@
+const start = Date.now();
 var state = true;
 var error = 0;
 var sudoku = [
@@ -11,44 +12,41 @@ var sudoku = [
   [3, 7, 0, 0, 9, 0, 1, 0, 0],
   [0, 0, 5, 1, 3, 0, 0, 0, 0],
 ];
-var matrix = [];
-for (let cN = 0; cN < 9; cN++) {
-  for (let cC = 0; cC < 81; cC++) {
-    let currentMatrix = [];
-    for (let celU = 0; celU < 81; celU++) {
-      if (celU == cC) {
-        currentMatrix[celU] = 1;
-      } else {
-        currentMatrix[celU] = 0;
+var baseMatrix = [];
+var mainMatrix = [];
+(function generate() {
+  for (let cN = 0; cN < 9; cN++) {
+    for (let cC = 0; cC < 81; cC++) {
+      let currentMatrix = [];
+      for (let celU = 0; celU < 81; celU++) {
+        if (celU == cC) {
+          currentMatrix[celU] = 1;
+        } else {
+          currentMatrix[celU] = 0;
+        }
+        if (celU == Math.floor(cC / 9) * 9 + cN) {
+          currentMatrix[celU + 81] = 1;
+        } else {
+          currentMatrix[celU + 81] = 0;
+        }
+        if (celU == (cC % 9) * 9 + cN) {
+          currentMatrix[celU + 162] = 1;
+        } else {
+          currentMatrix[celU + 162] = 0;
+        }
+        if (celU == (Math.floor((cC % 9) / 3) + 3 * Math.floor(Math.floor(cC / 9) / 3)) * 9 + cN) {
+          currentMatrix[celU + 243] = 1;
+        } else {
+          currentMatrix[celU + 243] = 0;
+        }
       }
+      baseMatrix.push(currentMatrix);
+      mainMatrix.push(currentMatrix);
     }
-    for (let rowU = 0; rowU < 81; rowU++) {
-      if (rowU == Math.floor(cC / 9) * 9 + cN) {
-        currentMatrix[rowU + 81] = 1;
-      } else {
-        currentMatrix[rowU + 81] = 0;
-      }
-    }
-    for (let colU = 0; colU < 81; colU++) {
-      if (colU == (cC % 9) * 9 + cN) {
-        currentMatrix[colU + 162] = 1;
-      } else {
-        currentMatrix[colU + 162] = 0;
-      }
-    }
-    for (let boxU = 0; boxU < 81; boxU++) {
-      if (
-        boxU ==
-        Math.floor((cC % 9) / 3) +
-          3 * Math.floor(Math.floor(cC / 9) / 3) * 9 +
-          cN
-      ) {
-        currentMatrix[boxU + 243] = 1;
-      } else {
-        currentMatrix[boxU + 243] = 0;
-      }
-    }
-    matrix.push(currentMatrix)
   }
-}
-console.log(matrix);
+})();
+(function analyse() {
+  console.log(mainMatrix.indexOf(mainMatrix[256]))
+})();
+const end = Date.now();
+console.log(`Execution time: ${end - start} ms`);
